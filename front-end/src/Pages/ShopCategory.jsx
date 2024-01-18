463d80cbc38e71bcb767a98090be06764fd0cfc0
Tsion <tsionwendmu12@gmail.com>
2024-01-18 15:24:51 -0500

    modified ShopCategory.jsx. Mapped out all the products.

--- front-end/src/Pages/ShopCategory.jsx
+++ front-end/src/Pages/ShopCategory.jsx
@@ -1,5 +1,7 @@
 import { useContext } from "react";
 import { ShopContext } from '../Context/ShopContext.jsx'
+import dropdown_icon from '/Users/zion/Documents/Mine/PalatePassion/front-end/src/Components/Assets /dropdown_icon.png'
+import Item from "../Components/Item/Item.jsx";
 
 const ShopCategory = (props) => {
     const {all_product} = useContext(ShopContext)
@@ -8,7 +10,25 @@ const ShopCategory = (props) => {
        
        
         <div className="shop-category">
-          
+            <img src={props.banner} alt="" />
+            <div className="shopcategory-indexSort">
+                <p>
+                    <span>Showing 1-12</span> out of 36 products
+                </p>
+                <div className="shopcategory-sort">
+                    Sort by <img src={dropdown_icon} alt="" />
+                </div>
+            </div>
+            <div className="shopcategory-products">
+                {all_product.map((item, i) =>
+                {
+                  if (props.category === item.category){
+                       return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+                  }  else{
+                    return null
+                  }
+                })}
+            </div>
         </div>
     );
 };
